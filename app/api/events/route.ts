af3cb413f861dd517efb93fe9c4e01dd3cb99cb6
Gull-e-zahra <gullzahra64@gmail.com>
2025-11-24 23:39:27 +1400

    route.ts updated

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,9 +1,19 @@
 import {NextRequest,NextResponse} from "next/server";
+import connectDB from "@/lib/mongodb";
 
 export async function POST(req:NextRequest){
 
     try{
+         await connectDB()
+        const formData =await req.formData();
+         let event;
 
+         try{
+             event=Object.fromEntries(formData.entries())
+         }
+         catch(e){
+               return NextResponse.json({message:'Invalid JSON data format'})
+         }
     }
     catch(e)
     {
